bac0466ca2f6f3eca9f71f7b6945cdcc1a4d059c
Felipe Christian Lautenschlager <felipelauten@gmail.com>
2022-10-23 02:26:08 +0200

    Removing comment

--- MultiMark/PreviewViewController.swift
+++ MultiMark/PreviewViewController.swift
@@ -33,16 +33,5 @@ class PreviewViewController: UIViewController {
             self.outputView.attributedText = attributedString
         }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
